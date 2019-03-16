c7838cecf4295370122cc3a3bdb21efd9876abec
Jakub Sapalski <coosheck@gmail.com>
2019-03-16 21:06:28 +0100

    Add example websocket server
    This is work in progress

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,30 @@
 import * as express from 'express';
 import * as request from 'request';
+import * as http from 'http';
+import * as WebSocket from 'ws';
 
 const app = express();
+const server = http.createServer(app);
+const ws = new WebSocket.Server({ server });
 const port = process.env.PORT || 80;
 const frontendUrl = process.env.FRONTEND || 'http://localhost:3000';
 
-app.listen(port, () => console.log(`Server running on ${port}!`));
+app.get('/map', (req, res) => {
+  res.send([]);
+});
+
+ws.on('connection', socket => {
+  console.log('connected');
+  socket.on('message', message => {
+    console.log('Received: %s', message);
+    socket.send(`Echo '${message}'`);
+  });
+  socket.send(JSON.stringify({ version: process.env.npm_package_version }));
+});
 
 app.get('*', (req, res) => {
   const url = frontendUrl + req.url;
   req.pipe(request(url)).pipe(res);
 });
+
+server.listen(port, () => console.log(`Server running on ${port}!`));
