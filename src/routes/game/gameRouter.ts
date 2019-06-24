bcc355bfbb7744c15784119435be649e8f4a5688
Jakub Sapalski <coosheck@gmail.com>
2019-06-24 15:57:22 +0200

    WIP

--- src/routes/game/gameRouter.ts
+++ src/routes/game/gameRouter.ts
@@ -5,6 +5,8 @@ import userMiddleware from '../user/userMiddleware';
 import message from './message';
 import editGame from './editGame';
 import getInvite from './getInvite';
+import joinGame from './joinGame';
+import getInviteInfo from './getInviteInfo';
 
 const gameRouter = (container: Container): Router => {
   const router = Router();
@@ -15,6 +17,8 @@ const gameRouter = (container: Container): Router => {
   router.post('/message/:gameId', message(container));
   router.post('/rename/:gameId', editGame(container));
   router.get('/invite/:gameId', getInvite(container));
+  router.get('/inviteInfo/:inviteToken', getInviteInfo(container));
+  router.post('/join', joinGame(container));
 
   return router;
 };
